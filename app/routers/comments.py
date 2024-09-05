183ce2268d4e2ca368021a2b28b4268a9e5ede8f
Ali-Bashar <zainzawar72@gmail.com>
2024-09-06 01:26:56 +0500

    Migrated comments table into postgresql db

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -8,5 +8,10 @@ from . import Oauth2
 router = APIRouter(tags=['comments'], prefix='/comments')
 
 router.post('/')
-def commenting(db:Session=Depends(database.get_db)):
-    pass
+def commenting(comment:schemas.Comments,db:Session=Depends(database.get_db),
+               current_user: str = Depends(Oauth2.get_current_user)):
+    
+    post_id = db.query(models.Post).filter(models.Post.post_id == comment.post_id).first()
+
+    if not post_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post id {comment.post_id} was not found")
